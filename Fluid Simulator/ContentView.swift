b9624047d6e29474f5b74c644df2ef0620ca4ed5
yjkh17 <yj.kh17@gmail.com>
2025-06-06 09:53:13 +0300

    Initial Commit

---
+++ Fluid Simulator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fluid Simulator
+//
+//  Created by Yousef Jawdat on 06/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fluid Simulator/Fluid_SimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fluid_SimulatorApp.swift
+//  Fluid Simulator
+//
+//  Created by Yousef Jawdat on 06/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct Fluid_SimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
